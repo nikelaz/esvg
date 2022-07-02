0800b0acb06550df20b992f6a702f9e336b5e0d2
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2022-07-02 12:58:48 +0300

    refactor svgo adapter tests

--- test/svgo-adapter.spec.ts
+++ test/svgo-adapter.spec.ts
@@ -16,7 +16,16 @@ describe('optimize()', () => {
   });
 
   test('prefixes ids and classNames', () => {
-    const svg = '<svg viewBox="0 0 100 100"><clipPath id="myClip"><circle cx="40" cy="35" r="35"/></clipPath><path id="heart" d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"/><use clip-path="url(#myClip)" href="#heart" fill="red"/></svg>';
+    const svg = `
+      <svg viewBox="0 0 100 100">
+        <clipPath id="myClip">
+          <circle cx="40" cy="35" r="35"/>
+        </clipPath>
+        <path id="heart" d="M10,30 A20,20,0,0,1,50,30 A20,20,0,0,1,90,30 Q90,60,50,90 Q10,60,10,30 Z"/>
+        <use clip-path="url(#myClip)" href="#heart" fill="red"/>
+      </svg>
+    `;
+
     const result = SVGOAdapter.optimize(svg);
     
     expect(result).toContain('id=');
